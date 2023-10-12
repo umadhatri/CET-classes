d4408397b894871af6046521b4558453a4b2f5ac
Uma Dhatri <97341880+umadhatri@users.noreply.github.com>
2023-10-12 08:08:30 +0000

    added constructor

--- objectOrientedProgramming.cpp
+++ objectOrientedProgramming.cpp
@@ -10,7 +10,17 @@ class flight
         long fare, amount, tax, disc, net;
         int nTickets;
     public:
-        void getData()
+
+        flight()
+        {
+            flightNumber = "IND621";
+            sourceLoc = "HYD";
+            destinationLoc = "DEL";
+            fare = 15000;
+            nTickets = 3;
+        }
+
+        /*void getData()
         {
             cout << "Enter Flight Number: "<<endl;
             cin >> flightNumber;
@@ -22,13 +32,13 @@ class flight
             cin >> nTickets;
             cout << "Enter Fare: " << endl;
             cin >> fare;
-        }
+        }*/
         void calculate()
         {
             amount = (fare*nTickets);
             tax = amount*(18/100);
             disc = (amount*5)/100;
-            net - (amount+tax) - disc;
+            net = (amount+tax) - disc;
         }
         void putData()
         {
@@ -44,7 +54,7 @@ int main()
     cout << "\n\n";
 
     flight flt;
-    flt.getData();
+    //flt.getData();
     flt.calculate();
     flt.putData();
 
