a54a868b3c6d55c6b56cd8666c208a067d32155b
Uma Dhatri <97341880+umadhatri@users.noreply.github.com>
2024-02-21 13:45:23 +0530

    Added Merge Sort

---
+++ mergeSort.py
@@ -0,0 +1,32 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+        mergeSort(L)
+        mergeSort(R)
+        i = j = k = 0
+        while((i<len(L)) and (j<len(R))):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+                k += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            k += 1
+            i += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            k += 1
+            j += 1
+
+    return arr
+
+arr = [77, 16, 19, 101, -1]
+print(mergeSort(arr))
